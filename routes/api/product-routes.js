b001ad40a9c8246b347235244110bcf9d920f348
Juawito <santiagojuan311@gmail.com>
2021-06-11 21:14:19 -0400

    adding delete route for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -109,8 +109,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deletedProduct = await Product.destroy({where: { id: req.params.id}});
+    if(!deletedProduct) {
+      res.status(404).json({message: 'The product is not in our database'})
+      return
+    }
+    res.status(200).json(deletedProduct);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+  
 });
 
 module.exports = router;
