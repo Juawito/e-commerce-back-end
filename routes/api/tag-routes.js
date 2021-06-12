ebd00a199d7a8d1727ed7a3a2cd31db6b4d0b97f
Juawito <santiagojuan311@gmail.com>
2021-06-11 22:07:29 -0400

    addind delete route for tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -63,8 +63,17 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deletedTag = await Tag.destroy({where: { id: req.params.id }});
+    if (!deletedTag) {
+      return res.status(404).json({ message: 'The tag is not in the database'})
+    }
+    res.status(200).json(deletedTag);
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 module.exports = router;
